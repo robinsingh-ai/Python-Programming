5940d28cf63236bafa9168d62300893bbba1c630
Robin Singh <Robin25Tech@gmail.com>
2020-02-08 21:50:14 +0530

    CALC
    ADDED

---
+++ SIMPLE_CALCULATOR.py
@@ -0,0 +1,27 @@
+#Write a Program to make simple calculator using if statements
+print("1. Addition");
+print("2. Subtraction");
+print("3. Multiplication");
+print("4. Division");
+print("5. Exit");
+choice = int(input("Enter your choice: "));
+if (choice>=1 and choice<=4):
+    print("Enter two numbers: ");
+    num1 = int(input());
+    num2 = int(input());
+    if choice == 1:
+     res = num1 + num2;
+     print("Result = ", res);
+    elif choice == 2:
+     res = num1 - num2;
+     print("Result = ", res);
+    elif choice == 3:
+     res = num1 * num2;
+     print("Result = ", res);
+    else:
+     res = num1 / num2;
+     print("Result = ", res);
+elif choice == 5:
+    exit();
+else:
+    print("Wrong input..!!");
