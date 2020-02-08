900a643acfbceb0e8a147e7852dea2ae2c609268
Robin Singh <Robin25Tech@gmail.com>
2020-02-08 21:50:41 +0530

    MERGING TWO LIST AND SORTING
    ADDED

---
+++ SORTING_MERGING.py
@@ -0,0 +1,8 @@
+#SORTING AND MERGING A LIST
+list1=[1,3,6,88,96,4,46,5]
+list2=[69,68,67,66,65]
+print("list 1 is",list1,"\nlist2 is",list2)
+list3=list1+list2
+print("list after concadinating is",list3)
+list3.sort()
+print("Sorted list is",list3)
