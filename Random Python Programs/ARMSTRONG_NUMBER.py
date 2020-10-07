1a05154fa2e4ab1d8e77e3d96c8fcfdbc772f6c3
Gourav-KP <72120283+Gourav-KP@users.noreply.github.com>
2020-10-07 15:52:52 +0530

    'voabvovj
    awrgbrhznryzee

--- Random Python Programs/ARMSTRONG_NUMBER.py
+++ Random Python Programs/ARMSTRONG_NUMBER.py
@@ -14,6 +14,6 @@ while temp > 0:
    temp //= 10
 # display the result
 if num == sum:
-   print(num,"is an Armstrong number")
+   print(num,"is an a number")
 else:
-   print(num,"is not an Armstrong number")
+   print(num,"is not an a number")
